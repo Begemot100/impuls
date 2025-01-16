d766d51c29cd77b0d89892a8d95a25fd7df0496c
Begemot100 <76606767+Begemot100@users.noreply.github.com>
2025-01-16 20:22:24 +0100

    Update config.py
    delete api key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_hard_to_guess_string'
-    CRM_API_KEY = '35fdd66e-7f4e-4281-81ae-2a0411344a9e'
+    CRM_API_KEY = ''
     CRM_BASE_URL = 'https://fotona.crmexpert.md'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///crmimpuls.db'  # Укажите путь к базе данных
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///crmimpuls.db'  
     SQLALCHEMY_TRACK_MODIFICATIONS = False
