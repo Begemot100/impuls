921460cbbbf9b922a9a69440fa14b5206aa4683c
Begemot100 <76606767+Begemot100@users.noreply.github.com>
2025-02-08 01:38:53 +0100

    Update service.py

--- models/service.py
+++ models/service.py
@@ -7,5 +7,4 @@ class Service(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     service_type = db.Column(db.String(10), nullable=True)
-    duration = db.Column(db.Integer, nullable=True)  # Продолжительность в минутах
-    # Добавьте дополнительные поля для услуг
+    duration = db.Column(db.Integer, nullable=True)  
